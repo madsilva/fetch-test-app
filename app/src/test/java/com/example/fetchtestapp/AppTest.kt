db20bae24ee0098795a2109e72ca67421a305805
madsilva <mad.silva.ic@gmail.com>
2023-04-05 20:21:45 -0500

    Clean up unit tests.

--- app/src/test/java/com/example/fetchtestapp/AppTest.kt
+++ app/src/test/java/com/example/fetchtestapp/AppTest.kt
@@ -4,19 +4,31 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class AppTest {
+    private val testHashMap = hashMapOf<Int, MutableList<ListItem>>(
+        1 to mutableListOf<ListItem>(ListItem(1, 1, "")),
+        2 to mutableListOf<ListItem>(ListItem(3, 2, ""), ListItem(4, 2, ""))
+    )
     @Test
     fun testGetRecyclerDataset (){
-        val input = hashMapOf<Int, MutableList<ListItem>>(
-            1 to mutableListOf<ListItem>(ListItem(1, 1, ""))
-        )
+        val input = testHashMap
         val expectedOutput = mutableListOf<Any>(
             ListID(1),
-            ListItem(1, 1, "")
+            ListItem(1, 1, ""),
+            ListID(2),
+            ListItem(3, 2, "",),
+            ListItem(4, 2, "")
         )
         assertEquals(expectedOutput, getRecyclerDataset(input))
     }
 
-    @Test fun testRecyclerViewRender () {
-
+    @Test
+    fun testGetListIdMap () {
+        val input = listOf<ListItem> (
+            ListItem(3, 2, ""),
+            ListItem(4, 2, ""),
+            ListItem(1, 1, "")
+                )
+        val expectedOutput = testHashMap
+        assertEquals(expectedOutput, getListIdMap(input))
     }
 }
